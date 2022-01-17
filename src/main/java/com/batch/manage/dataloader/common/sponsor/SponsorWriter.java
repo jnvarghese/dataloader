4739a2458a697e45dfc027d3664ae6f217fb701e
jnvarghese <jobsjinu@gmail.com>
2022-01-17 12:30:57 -0500

    fixed the error while loading sponsor only

--- src/main/java/com/batch/manage/dataloader/common/sponsor/SponsorWriter.java
+++ src/main/java/com/batch/manage/dataloader/common/sponsor/SponsorWriter.java
@@ -21,10 +21,8 @@ public class SponsorWriter implements ItemWriter<Sponsor> {
     
     @Override
     public void write(List<? extends Sponsor> items) throws Exception {
-        //LOGGER.info("Received the information of {} students", items.size());
-
-       items.forEach(i -> LOGGER.debug("Received the information of a Sponsor: {}", i.getFirstName(), i.getLastName(),
-    		   i.getErn().getPaymentDate(), i.getErn().getEffectiveDate(), i.getErn().getSponsees(), i.getSponsorMaxOuts()));
+       //LOGGER.info("Received the information of {} students", items.size());
+       items.forEach(i -> LOGGER.debug("Received the information of a Sponsor: {}", i.getFirstName(), i.getLastName()));
         
         for(Sponsor dto : items) {
         	//receiptDAO.save(dto.getErn().getReceipt());
